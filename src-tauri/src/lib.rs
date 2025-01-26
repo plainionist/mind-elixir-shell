2e79135820c6dd3567c9db3427ef0a7e79d21039
plainionist <mail@plainionist.net>
2025-01-26 15:01:16 +0100

    File menu defined

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,14 +1,37 @@
-// Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
-#[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
-}
+use tauri::menu::*;
+use tauri::Emitter;
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
-        .plugin(tauri_plugin_opener::init())
-        .invoke_handler(tauri::generate_handler![greet])
+        .setup(|app| {
+            let file_menu = SubmenuBuilder::new(app, "File")
+                .text("open", "Open")
+                .text("save", "Save")
+                .text("export-svg", "Export SVG")
+                .separator()
+                .quit()
+                .build()?;
+
+            let menu = MenuBuilder::new(app).item(&file_menu).build()?;
+
+            app.set_menu(menu)?;
+
+            app.on_menu_event(move |app, event| {
+                if event.id() == "open" {
+                    app.emit("menu:open", {})
+                        .expect("Failed to emit event 'menu:open'");
+                } else if event.id() == "save" {
+                    app.emit("menu:save", {})
+                        .expect("Failed to emit event 'menu:save'");
+                } else if event.id() == "export-svg" {
+                    app.emit("menu:export-svg", {})
+                        .expect("Failed to emit event 'menu:export-svg'");
+                }
+            });
+
+            Ok(())
+        })
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
